4559d379815b75bd7e9251fa277d341e1764929e
Graham Batzler <batzlerg@gmail.com>
2014-02-16 02:02:03 -0500

    About page complete

--- routes.js
+++ routes.js
@@ -1,3 +1,7 @@
+// ================================================================================
+// CONFIG =========================================================================
+// ================================================================================
+
 var passport = require('passport');
 var models = require('./models/account');
   var Account = models.Account;
@@ -7,19 +11,20 @@ var loginControl = require('connect-ensure-login');
 
 module.exports = function (app) {
 
-  app.get('/hello', function(req, res){
-    var dataEJS = {};
-    dataEJS.page_title = 'My Beautiful Title';
-    dataEJS.page_body = 'My Beautiful Content';
-    res.render('layout.ejs', dataEJS);
-  });
+// ================================================================================
+// INDEX ==========================================================================
+// ================================================================================
 
   app.get('/', function (req, res) {
     res.render('index.ejs', { user : req.user, page_title: 'StudyBuddy: connect with your classmates'});
   });
 
+// ================================================================================
+// REGISTER =======================================================================
+// ================================================================================
+
   app.get('/register', function(req, res) {
-      res.render('register.ejs', { msg: "", page_title: 'Register for StudyBuddy'});
+      res.render('register.ejs', { msg: "", page_title: 'StudyBuddy | Register'});
   });
 
   app.post('/register', function(req, res) {
@@ -27,7 +32,7 @@ module.exports = function (app) {
     if (req.body.email.indexOf('@') > -1 && req.body.email.substr(req.body.email.length - 9) === 'upenn.edu') {
       Account.register(new Account({ username : req.body.email }), req.body.password, function(err, account) {
         if (err) {
-            return res.render('register.ejs', { msg: "That email already exists. Try again." });
+            return res.render('register.ejs', { msg: "An account is already linked to that email" });
         }
 
         passport.authenticate('local')(req, res, function () {
@@ -42,6 +47,10 @@ module.exports = function (app) {
       res.render('register.ejs', { msg: 'Valid Penn address required', page_title: 'StudyBuddy | Register' });
     }
   });
+
+// ================================================================================
+// LOGIN/OUT ======================================================================
+// ================================================================================
 
   app.get('/login', function(req, res) {
     console.log("Routing a request for: /login");
@@ -62,13 +71,17 @@ module.exports = function (app) {
   // app.get('/canvas',
   //   passport.authorize(''));
 
+// ================================================================================
+// STATIC =========================================================================
+// ================================================================================
+
   app.get('/user', loginControl.ensureLoggedIn('/login'),
     function(req,res){
       res.render('user.ejs', {user: req.user})
-    });
+  });
 
-  app.get('/ping', function(req, res){
-      res.send("pong!", 200);
+  app.get('/about', function(req,res){
+      res.render('about.ejs', { page_title: 'StudyBuddy | About' });
   });
 
 };
